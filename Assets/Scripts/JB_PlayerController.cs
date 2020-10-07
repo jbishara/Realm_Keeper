f452404da731933c504fd2be96a61a63c3de77b7
James <j.bishara@hotmail.com>
2020-10-07 12:28:34 +1100

    Begin Prototype

---
+++ Assets/Scripts/JB_PlayerController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JB_PlayerController : MonoBehaviour
+{
+    [SerializeField] private Transform projectileSpawnPoint;
+    [SerializeField] private GameObject rockPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Abilities();
+    }
+
+    private void Abilities()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            // ability one
+            AbilityOne();
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            // ability two
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            // ability three
+        }
+    }
+
+    private void AbilityOne()
+    {
+        Instantiate(rockPrefab, projectileSpawnPoint.position, projectileSpawnPoint.rotation);
+    }
+}
---
+++ Assets/Scripts/JB_RockProjectile.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JB_RockProjectile : MonoBehaviour
+{
+    private Rigidbody rigidBody;
+
+    [SerializeField] private float force;
+    [SerializeField] private float speed;
+    [SerializeField] private float turnSpeed;
+    [SerializeField] private Vector3 downRotation;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+        rigidBody.AddForce(transform.forward * force);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //transform.forward = Vector3.RotateTowards(transform.forward, downRotation, turnSpeed * Time.deltaTime, 1.0f);
+
+        //rigidBody.velocity = transform.forward * speed;
+
+        
+        
+    }
+}
