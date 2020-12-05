158c8dba7c4c00531deaa8fdff9e7db65313ed2e
Jacqueline H <1015612@student.sae.edu.au>
2020-12-05 16:19:17 +1100

    Fixed issues with exit to menu button and started health bar function
    + Created Health Bar script to show player health on screen. 
    ~ Edited HUDPanel prefab and connected the new script.
    ~ Fixed Pause Menu scripts so they go to they now go to the menu when the exit to menu button is pushed.
    
    NOTE: The Health Bar script hasn't been connected to JB_HealthComponet as I don''t fully understand how to connect this (I did try but the code said to couldn't get the refernce).

---
+++ Assets/Scripts/UI/JH_HealthBar.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class JH_HealthBar : MonoBehaviour
+{
+    public Slider healthBarSlider;              // Connects to HealthBar
+    public Gradient healthBarGradient;
+    public Image healthBarFill;                 // Connects to HealthBarFill
+
+    // NOTE: Will need to refernce this in JB_HealthComponent Script. 
+    // This hasn't been done due to not full understanding how the scrip works or where the correct places to reference this is.
+
+    public void SetMaxHealth(float health)
+    {
+        healthBarSlider.maxValue = health;
+        healthBarSlider.value = health;
+
+        healthBarFill.color = healthBarGradient.Evaluate(1f);
+    }
+
+    public void SetHealth(float health)
+    {
+        healthBarSlider.value = health;
+
+        healthBarFill.color = healthBarGradient.Evaluate(healthBarSlider.normalizedValue);
+    }
+}
--- Assets/Scripts/UI/JH_PauseMenuGeneral.cs
+++ Assets/Scripts/UI/JH_PauseMenuGeneral.cs
@@ -69,7 +69,7 @@ public class JH_PauseMenuGeneral : MonoBehaviour
 
     public void ExitToMenu()
     {
-        SceneManager.LoadScene("");                 // LOADS Main Menu Scene
+        SceneManager.LoadScene("Main_Menu");        // LOADS Main Menu Scene
         Debug.Log("Quit to Main Menu");
     }
 }
