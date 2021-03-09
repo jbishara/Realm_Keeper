fca2fa46c074776ff4add979dc5a1bea40773f05
ruggot <50566524+ruggot@users.noreply.github.com>
2021-03-09 20:19:11 +1100

    Making a playable protype
    + adding navmesh to the levels
    + disable a enemy in cave level because it push the player of with it's collider, and even though its collider was not that big
    + made some simple script that makes the player spawn back on the platform if they fall out of level

---
+++ Assets/Scripts/LD_NextLevel.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LD_NextLevel : MonoBehaviour
+{
+    void OnCollisionEnter(Collision collision)
+    {
+        //gameObject.transform.position = falloutSpawner;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
---
+++ Assets/Scripts/LD_falloutscript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LD_falloutscript : MonoBehaviour
+{
+    public GameObject falloutSpawner;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    
+    void OnCollisionEnter(Collision collision)
+    {
+        //gameObject.transform.position = falloutSpawner;
+    }
+}
