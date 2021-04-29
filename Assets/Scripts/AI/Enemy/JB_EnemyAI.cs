0003f7eec9d0bd02df274bed3168c8256f73adb7
jbishara <51963978+jbishara@users.noreply.github.com>
2021-04-29 19:01:45 +1000

    Began new AI system

---
+++ Assets/Scripts/AI/Enemy/JB_EnemyAI.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class JB_EnemyAI : MonoBehaviour
+{
+    public float lookRadius = 10f;
+    public AiEnemyTypes enemyType;
+
+    private Transform player;
+    private NavMeshAgent agent;
+
+    private void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        player = Master_Script.instance.player.transform;
+    }
+
+    private void Update()
+    {
+        float distance = Vector3.Distance(player.position, transform.position);
+
+        if(distance <= lookRadius)
+        {
+            agent.SetDestination(player.position);
+
+            if(distance <= agent.stoppingDistance)
+            {
+                // attack target
+                // begin attack animation
+                FaceTarget();
+            }
+        }
+    }
+
+    private void FaceTarget()
+    {
+        Vector3 direction = (player.position - transform.position).normalized;
+        Quaternion lookRotation = Quaternion.LookRotation(new Vector3(direction.x, 0, direction.z));
+        transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * 5f);
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, lookRadius);
+    }
+}
---
+++ Assets/Scripts/AI/Enemy/JB_EnemyAttack.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JB_EnemyAttack : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void AttackPlayer(AiEnemyTypes enemyType)
+    {
+        // play animation
+        // trigger dmg dealt to the player based off
+
+    }
+}
