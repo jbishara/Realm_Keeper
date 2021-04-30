10d01b615c73af87d224c4f8e59d8de58ff2104a
LD <1014428@student.sae.edu.au>
2021-04-30 07:30:46 +0200

    Enemy plays audio files with they animations
    ~ Bagooblin, Troll, Elemental & Stone Golem has animations and audio files for death and take damages added. Missing these enemy's attack SFX then missing bosses animations to add they audio files to them

--- Assets/Scripts/Audio/LD_AudioManager.cs
+++ Assets/Scripts/Audio/LD_AudioManager.cs
@@ -10,8 +10,6 @@ public class LD_AudioManager : MonoBehaviour
     // makes stores a refences to it self
     public static LD_AudioManager instance;
 
-    private int whichLevel;
-
     // when script becomes created do this
     void Awake()
     {
@@ -52,5 +50,234 @@ public class LD_AudioManager : MonoBehaviour
         }
         s.source.Play();
         
+    }
+
+    // Bagoblin SFX functions
+    void BagoblinTakeDamages()
+    {
+        int min = 0;
+        int max = 3;
+        int whatAudioFile = UnityEngine.Random.Range(min, max);
+        switch (whatAudioFile)
+        {
+            case 0:
+                Play("Bagoblin_INJ_Grunt");
+                break;
+            case 1:
+                Play("Bagoblin_INJ_Short");
+                break;
+            case 2:
+                Play("Bagoblin_INJ_Loud");
+                break;
+        }
+    }
+    void BagoblinDie()
+    {
+        int min = 0;
+        int max = 3;
+        int whatAudioFile = UnityEngine.Random.Range(min, max);
+        switch (whatAudioFile)
+        {
+            case 0:
+                Play("Bagoblin_Die_Long");
+                break;
+            case 1:
+                Play("Bagoblin_Die_Growl");
+                break;
+        }
+    }
+
+    // Troll SFX functions
+    void trollTakeDamages()
+    {
+        int min = 0;
+        int max = 3;
+        int whatAudioFile = UnityEngine.Random.Range(min, max);
+        switch (whatAudioFile)
+        {
+            case 0:
+                Play("Troll_INJ_Grunt");
+                break;
+            case 1:
+                Play("Troll_INJ_Ooh");
+                break;
+        }
+    }
+    void trollTakeDie()
+    {
+        int min = 0;
+        int max = 4;
+        int whatAudioFile = UnityEngine.Random.Range(min, max);
+        switch (whatAudioFile)
+        {
+            case 0:
+                Play("Troll_DIE_Short");
+                break;
+            case 1:
+                Play("Troll_DIE_Long");
+                break;
+            case 2:
+                Play("Troll_DIE_Grunt");
+                break;
+        }
+    }
+
+    // Cyclops SFX functions
+    void cyclopsTakeDamages()
+    {
+        // missing animation to put this function into
+        int min = 0;
+        int max = 4;
+        int whatAudioFile = UnityEngine.Random.Range(min, max);
+        switch (whatAudioFile)
+        {
+            case 0:
+                Play("Cyclops_INJ_Shh");
+                break;
+            case 1:
+                Play("Cyclops_INJ_Sss");
+                break;
+            case 2:
+                Play("Cyclops_INJ_Nagh");
+                break;
+        }
+    }
+    void cyclopsDie()
+    {
+        // missing animation to put this function into
+        int min = 0;
+        int max = 5;
+        int whatAudioFile = UnityEngine.Random.Range(min, max);
+        switch (whatAudioFile)
+        {
+            case 0:
+                Play("Cyclops_DIE_Nargh");
+                break;
+            case 1:
+                Play("Cyclops_DIE_Nsh");
+                break;
+            case 2:
+                Play("Cyclops_DIE_Sss");
+                break;
+            case 3:
+                Play("Cyclops_DIE_Shh");
+                break;
+        }
+    }
+
+    // Elemental SFX functions
+    void elementalTakeDamges()
+    {
+        // missing animation to put this function into
+        int min = 0;
+        int max = 4;
+        int whatAudioFile = UnityEngine.Random.Range(min, max);
+        switch (whatAudioFile)
+        {
+            case 0:
+                Play("ELE_INJ_Ooh");
+                break;
+            case 1:
+                Play("ELE_INJ_Ohh");
+                break;
+            case 2:
+                Play("ELE_DIE_Ahh");
+                break;
+        }
+    }
+    void elementalDie()
+    {
+        // missing animation to put this function into
+        int min = 0;
+        int max = 4;
+        int whatAudioFile = UnityEngine.Random.Range(min, max);
+        switch (whatAudioFile)
+        {
+            case 0:
+                Play("ELE_DIE_Ahh");
+                break;
+            case 1:
+                Play("ELE_DIE_Ooh");
+                break;
+            case 2:
+                Play("ELE_DIE_Ohh");
+                break;
+        }
+    }
+
+    // stone Golem SFX functions
+    void stoneGolemTakeDamges()
+    {
+        int min = 0;
+        int max = 3;
+        int whatAudioFile = UnityEngine.Random.Range(min, max);
+        switch (whatAudioFile)
+        {
+            case 0:
+                Play("StoneGolem_INJ_Ugh");
+                break;
+            case 1:
+                Play("StoneGolem_INJ_Grunt");
+                break;
+        }
+    }
+    void stoneGolemDie()
+    {
+        int min = 0;
+        int max = 4;
+        int whatAudioFile = UnityEngine.Random.Range(min, max);
+        switch (whatAudioFile)
+        {
+            case 0:
+                Play("StoneGolem_DIE_Ho");
+                break;
+            case 1:
+                Play("StoneGolem_DIE_Ugh");
+                break;
+            case 2:
+                Play("StoneGolem_DIE_Grunt");
+                break;
+        }
+    }
+
+    // crystalGolem SFX functions
+    void crystaGolemTakeDamges()
+    {
+        int min = 0;
+        int max = 4;
+        int whatAudioFile = UnityEngine.Random.Range(min, max);
+        switch (whatAudioFile)
+        {
+            case 0:
+                Play("CrystalGolem_INJ_Rah");
+                break;
+            case 1:
+                Play("CrystalGolem_INJ_Oh");
+                break;
+            case 2:
+                Play("CrystalGolem_INJ_Ree");
+                break;
+        }
+    }
+    void crystalGolemDie()
+    {
+        int min = 0;
+        int max = 5;
+        int whatAudioFile = UnityEngine.Random.Range(min, max);
+        switch (whatAudioFile)
+        {
+            case 0:
+                Play("CrystalGolem_DIE_Rugh");
+                break;
+            case 1:
+                Play("CrystalGolem_DIE_Double");
+                break;
+            case 2:
+                Play("CrystalGolem_DIE_Triple");
+                break;
+            case 3:
+                Play("CrystalGolem_DIE_Ahh");
+                break;
+        }
     }
 }
