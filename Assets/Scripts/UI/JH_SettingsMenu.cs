17edbcb50f3c8bc85a573a58cecbdcb47355fad6
Jacqueline <jacqueline.holdaway@gmail.com>
2021-05-04 13:12:49 +1000

    Created Settings Menu Script
    + Created Settings Menu script (not implemented yet)
    + Added character images to character select

---
+++ Assets/Scripts/UI/JH_SettingsMenu.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class JH_SettingsMenu : MonoBehaviour
+{
+    public AudioMixer audioMixer;
+
+    public void SetVolume(float volume)
+    {
+        audioMixer.SetFloat("masterVolume", volume);
+    }
+
+    public void FullScreen(bool isFullscreen)
+    {
+        Screen.fullScreen = isFullscreen;
+        Debug.Log("THIS IS FULL SCREEN NOW");
+    }
+}
